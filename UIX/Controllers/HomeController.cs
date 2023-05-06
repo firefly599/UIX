f1f7777702e6e3ba9913d7072173bed1ce6f2783
Nikolai <nikolaivalkamo1@gmail.com>
2023-05-06 19:53:50 +0100

    Add project files.

---
+++ UIX/Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace UIX.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult LogInUser()
+        {
+            string username = Request.Form["username"];
+            string password = Request.Form["password"];
+
+            DataManager.Login(username, password);
+            return RedirectToAction("Home");
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignUpUser()
+        {
+            string username = Request.Form["username"];
+            string password = Request.Form["password"];
+            string email = Request.Form["email"];
+            string firstname = Request.Form["firstname"];
+            string lastname = Request.Form["lastname"];
+
+            DataManager.SignUp(username, password, email, firstname, lastname);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Meeting()
+        {
+            return View();
+        }
+
+        public IActionResult CreateMeeting()
+        {
+            ViewData["staff"] = Request.Form["staff"];
+            return View();
+        }
+
+        public IActionResult SaveMeeting()
+        {
+            string student = DataManager.activeUser.Name;
+            string staff = Request.Form["staff"];
+            int day = Convert.ToInt32(Request.Form["day"]);
+            int month = Convert.ToInt32(Request.Form["month"]);
+            int hour = Convert.ToInt32(Request.Form["hour"]);
+            int minute = Convert.ToInt32(Request.Form["minute"]);
+            DataManager.CreateMeeting(student, staff, day, month, hour, minute);
+
+            return RedirectToAction("Meeting");
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+        public IActionResult SendMsg()
+        {
+            int id = Convert.ToInt32(Request.Form["id"]);
+            ViewData["id"] = id;
+            return View();
+        }
+
+        public IActionResult SaveMsg()
+        {
+            int id = Convert.ToInt32(Request.Form["id"]);
+            string user = DataManager.activeUser.Name;
+            string msg = Request.Form["msg"];
+            DataManager.messages[id].AddMsg(msg, user);
+
+            return RedirectToAction("Messages");
+        }
+
+        public IActionResult NewMsg()
+        {
+            return View();
+        }
+
+        public IActionResult CreateMsg()
+        {
+            string username = DataManager.activeUser.Name;
+            string text = Request.Form["msg"];
+            string staff = Request.Form["staff"];
+            DataManager.CreateMessage(username, staff, text);
+            return RedirectToAction("Messages");
+        }
+
+        public IActionResult Profile()
+        {
+            return RedirectToAction("Home");
+        }
+
+        public IActionResult Logout()
+        {
+            DataManager.Logout();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ UIX/DataManager.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace UIX
+{
+    public class Person
+    {
+        public string Name;
+        public string FirstName;
+        public string LastName;
+        public string Title;
+        public string email;
+        public string staff;
+
+        public Person(string name, string title, string pstaff = null, string email=null, string firstname = null, string lastname = null)
+        {
+            Name = name;
+            if (firstname == null)
+            {
+                FirstName = name.Split(' ')[0];
+            }
+            else
+            {
+                FirstName = firstname;
+            }
+
+            if (lastname == null)
+            {
+                LastName = name.Split(' ')[0];
+            }
+            else
+            {
+                LastName = lastname;
+            }
+
+            Title = title;
+            email = email;
+            staff = pstaff;
+        }
+    }
+
+    public class Meeting
+    {
+        public int day;
+        public int month;
+        public int hour;
+        public int minute;
+
+        public string staff;
+        public string student;
+
+        public Meeting(int pday, int pmonth, int phour, int pminute, string pstaff, string pstudent)
+        {
+            day = pday;
+            month = pmonth;
+            hour = phour;
+            minute = pminute;
+            staff = pstaff;
+            student = pstudent;
+        }
+    }
+
+    public class Messages
+    {
+        public string staff;
+        public string student;
+        public int id;
+        public List<string> messages;
+
+        public Messages(string staff, string student, List<string> messages, int id)
+        {
+            this.staff = staff;
+            this.student = student;
+            this.messages = messages;
+            this.id = id;
+        }
+
+        public void AddMsg(string text, string sender)
+        {
+            string toAdd = sender + ": " + text;
+            messages.Add(toAdd);
+        }
+    }
+
+    public static class DataManager
+    {
+        static public List<Person> data = new ();
+        static public List<Meeting> meetings = new ();
+        static public List<Messages> messages = new ();
+        static public Person activeUser;
+
+        static public void FillData()
+        {
+            Person person = new Person("Mike Hobs", "Professor");
+            data.Add(person);
+            person = new Person("Cameron Atkinson", "Student", "Mike Hobs");
+            data.Add(person);
+            person = new Person("Rias Grimory", "Student", "Mike Hobs");
+            data.Add(person);
+            person = new Person("Ina Bael", "Student", "Yeong Jin");
+            data.Add(person);
+            person = new Person("Gu Obs", "Professor");
+            data.Add(person);
+            person = new Person("Siahn Park", "Student", "Gu Obs");
+            data.Add(person);
+            person = new Person("Kim Sung", "Student", "Yeong Jin");
+            data.Add(person);
+            person = new Person("Yeong Jin", "Professor");
+
+            data.Add(person);
+
+            meetings.Add(new Meeting(4, 5, 10, 20, "Mike Hobs", "Cameron Atkinson"));
+            meetings.Add(new Meeting(4, 7, 10, 40, "Mike Hobs", "Rias Grimory"));
+            meetings.Add(new Meeting(4, 5, 10, 20, "Gu Obs", "Siahn Park"));
+            meetings.Add(new Meeting(4, 5, 10, 20, "Yeong Jin", "Ina Bael"));
+            meetings.Add(new Meeting(4, 5, 10, 20, "Yeong Jin", "Kim Sung"));
+
+            List<string> mess = new List<string>();
+            mess.Add("Ina Bael: Hi when can i meet you?");
+            mess.Add("Mike Hobs: Let me check my calendar and get back to you ");   
+            messages.Add(new Messages("Mike Hobs", "Ina Bael", mess, 0));
+        }
+
+        static public void Login(string username, string password)
+        {
+            foreach (Person user in data)
+            {
+                if (username == user.Name)
+                {
+                    activeUser = user;
+                }
+            }
+        }
+
+        static public void SignUp(string username, string password, string email, string firstname, string lastname)
+        {
+            Person newUser = new (username, password, "null", email, firstname, lastname);
+            data.Add(newUser);
+            activeUser = newUser;
+        }
+
+        static public void Logout()
+        {
+            activeUser = null;
+        }
+
+        static public void CreateMeeting(string student, string staff, int day, int month, int hour, int minute)
+        {
+            meetings.Add(new Meeting(day, month, hour, minute, staff, student));
+        }
+
+        static public void CreateMessage(string user, string staff, string text)
+        {
+            List<string> mess = new List<string>();
+            mess.Add(text);
+            messages.Add(new Messages(user, staff, mess, messages.Count()));
+        }
+
+        static public List<Person> MeetingView()
+        {
+            return data;
+        }
+
+        static public List<Person> HomeView()
+        {
+            return data;
+        }
+    }
+}
